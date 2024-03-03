424e6ec221d04173f4acefa1515256477b171847
HristoAngeloff <66922355+Iccooo@users.noreply.github.com>
2024-03-03 20:41:14 +0200

    .

--- src/main/java/com/main/office/Controller/RegisterController.java
+++ src/main/java/com/main/office/Controller/RegisterController.java
@@ -2,12 +2,12 @@ package com.main.office.Controller;
 
 import com.main.office.Service.RegisterService;
 import com.main.office.Мodel.User;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
@@ -16,11 +16,12 @@ public class RegisterController {
     private RegisterService registerService;
 
     @GetMapping("/register")
-    public String showRegistrationForm() {
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User()); // Add an empty User object to the model
         return "register";
     }
 
-    @RequestMapping("/register")
+    @PostMapping("/register")
     public String registerUser(@ModelAttribute("user") User user) {
         boolean isUserRegistered = registerService.register(user);
 
@@ -32,3 +33,5 @@ public class RegisterController {
         }
     }
 }
+
+
